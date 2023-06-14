b55744a329519102eed42119fa45009f11aa1f79
BrandonLunaCastro <110911675+BrandonLunaCastro@users.noreply.github.com>
2023-06-14 18:21:24 -0300

    Update Post_home.js

--- API de anime/js/modulos/Post_home.js
+++ API de anime/js/modulos/Post_home.js
@@ -1,19 +1,23 @@
 
-
-export function posts(props){
-  let {posterImage,titles} = props;
+export function posts(props,id){
+  let {posterImage,titles,slug} = props;
 
   //console.log(posterImage,titles)
   let titulo = titles.en ? titles.en : titles.en_jp;
+
+    document.addEventListener("click",e =>{
+      if(!e.target.matches(".targeta a"))return false;
+      localStorage.setItem("PostId",e.target.dataset.id);
+    })
 
      return `
       <article class="targeta">
        
         <img src="${posterImage.original}"> 
         <h2>${titulo}</h2>
-        <p><a href="#">Ver mas...</a>.</p>
+        <p   ><a class="anime" data-id="${id}" href="#/${slug}">Ver mas...</a>.</p>
        
-      
+        
       </article>
     
     
