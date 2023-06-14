4c1b0653c17ddd2e4051e69e410d409a8143d368
BrandonLunaCastro <110911675+BrandonLunaCastro@users.noreply.github.com>
2023-06-14 18:21:27 -0300

    Update Anime_api.js

--- API de anime/js/modulos/Anime_api.js
+++ API de anime/js/modulos/Anime_api.js
@@ -1,15 +1,17 @@
 
-    ///https://kitsu.io/api/edge/anime?filter[text]=cowboy
+   
 let ANIMES = `https://kitsu.io/api/edge/anime`,
 SEARCH = `https://kitsu.io/api/edge/anime?filter[text]=`,
 page = 0,
-number_page = 1;
+number_page = 1,
+hash;
 
     export default{
         ANIMES,
         SEARCH,
         page,
-        number_page
+        number_page,
+        hash
     }
 
 
