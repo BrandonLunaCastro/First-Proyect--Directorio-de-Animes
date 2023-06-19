a4fe3ebffe1d811442eef35bb5f9b62e3acd3f72
BrandonLunaCastro <110911675+BrandonLunaCastro@users.noreply.github.com>
2023-06-19 16:04:23 -0300

    Update personaje.js

--- API de anime/js/modulos/personaje.js
+++ API de anime/js/modulos/personaje.js
@@ -2,17 +2,18 @@ export function personaje(props){
 
     let {canonicalName,image} = props
   
-    const $figure = document.createElement("figure"),
+    let $figure = document.createElement("figure"),
     $img = document.createElement("img"),
-    $figcaption = document.createElement("figcaption")
+    $figcaption = document.createElement("figcaption"),
+    imagen = image ? image.original : "assets/files/no-image-available.jpeg"
 
 
     $figure.appendChild($img)
     $figure.appendChild($figcaption)
     $figure.classList.add("character")
 
-    $img.setAttribute("src",`${image.original}`);
-    $img.alt = "imagen del personaje";
+    $img.setAttribute("src",`${imagen}`);
+    $img.alt = canonicalName;
     $figcaption.textContent = canonicalName;
 
     return $figure;
