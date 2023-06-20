55af652f6e01352791352a4ae7e285fb1422eacb
BrandonLunaCastro <blunacastro2003@gmail.com>
2023-06-20 11:31:53 -0300

    Create openTab.js

---
+++ API de anime/js/modulos/openTab.js
@@ -0,0 +1,21 @@
+export function openTab(event, section){
+    let $tabContent = document.querySelectorAll(".tabContent"),
+    $tabsLinks = document.querySelectorAll(".tabs-links");
+
+
+
+
+    for(let i = 0 ; i<$tabContent.length; i++){
+        $tabContent[i].style.display = "none";
+    }
+
+    for(let i = 0 ; i< $tabsLinks.length ; i++){
+            $tabsLinks[i].classList.remove("active")
+    }
+
+    document.getElementById(section).style.display = "block"
+    event.currentTarget.classList.add("active")
+
+}
+
+
