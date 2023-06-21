3ab6b7acf251baefe29becb801c01afd8772c945
BrandonLunaCastro <110911675+BrandonLunaCastro@users.noreply.github.com>
2023-06-21 17:43:21 -0300

    Create infiniteScroll.js

---
+++ API de anime/js/modulos/infiniteScroll.js
@@ -0,0 +1,10 @@
+import ajax from "./ajax";
+
+export async function infiniteScroll(url){
+    window.addEventListener("scroll",e => {
+        let {scrollTop,scrollHeight,clientHeight} = document.documentElement;
+
+        ajax
+
+    })
+}
