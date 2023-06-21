81e26d336da3793c8b5553dd95fadb8fedbbf2be
BrandonLunaCastro <110911675+BrandonLunaCastro@users.noreply.github.com>
2023-06-21 15:46:20 -0300

    Update episodios.js

--- API de anime/js/modulos/episodios.js
+++ API de anime/js/modulos/episodios.js
@@ -1,3 +1,13 @@
 export default function episodes(props){
-    console.log(props)
+    //console.log(props.attributes)
+    let {canonicalTitle,thumbnail,number} = props.attributes,
+    img = thumbnail ? thumbnail.original : "assets/files/no-image-available.jpeg",
+    titulo = canonicalTitle.slice(0,20)+"...";
+   
+    return `
+    <article class="container-episodes">
+        <img src="${img}" alt="${titulo}">
+        <p><strong>Episodio ${number}</strong> ${titulo}</p>
+    </article>
+    `;
 }
