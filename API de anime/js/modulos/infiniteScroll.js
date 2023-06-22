c903b4713a633e5c9b337624f31113c0ee51b79f
BrandonLunaCastro <blunacastro2003@gmail.com>
2023-06-22 09:52:16 -0300

    Update infiniteScroll.js

--- API de anime/js/modulos/infiniteScroll.js
+++ API de anime/js/modulos/infiniteScroll.js
@@ -2,9 +2,27 @@ import ajax from "./ajax";
 
 export async function infiniteScroll(url){
     window.addEventListener("scroll",e => {
-        let {scrollTop,scrollHeight,clientHeight} = document.documentElement;
+        let {scrollTop,scrollHeight,clientHeight} = document.documentElement,
+        direccionURL,
+        id = localStorage.getItem("PostId"),
+        page=0
 
-        ajax
+        if(location.hash === "#/personajes"){
+            page+=20
+            direccionURL = `https://kitsu.io/api/edge/anime/${id}/characters?page[limit]=20&page[offset]=${page}`
+        }else if(location.hash === "#/episodios"){
+            page
+        }
 
+        if(scrollTop + clientHeight >= scrollHeight){
+            ajax({
+                url: direccionURL,
+                cbSuccess:(personajes) =>{
+    
+                }
+            })
+    
+        }
+        
     })
 }
