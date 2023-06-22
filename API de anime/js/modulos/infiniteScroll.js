007552bc305c3cdec491ee8ce1964b9fc8f15c8f
BrandonLunaCastro <blunacastro2003@gmail.com>
2023-06-22 12:06:21 -0300

    Update infiniteScroll.js

--- API de anime/js/modulos/infiniteScroll.js
+++ API de anime/js/modulos/infiniteScroll.js
@@ -11,29 +11,36 @@ export async function infiniteScroll(){
         id = localStorage.getItem("PostId"),
         $tabcontentPj = document.getElementById("personajesContent"),
         $tabcontentEp = document.getElementById("episodiosContent")
-
-        Anime_api.page = 0
-
-        if(location.hash === "#/personajes"){
-            Anime_api.page+=20
+       console.log(Anime_api.page)
+       
+        if(scrollTop + clientHeight >= scrollHeight){
+           Anime_api.page+=20
+        
+         if(location.hash === "#/personajes"){    
             direccionURL = `https://kitsu.io/api/edge/anime/${id}/characters?page[limit]=20&page[offset]=${Anime_api.page}`
             contenido = function showPersonaje(){
 
             }
         }else if(location.hash === "#/episodios"){
-            Anime_api.page+=20
+            
             direccionURL = `https://kitsu.io/api/edge/anime/${id}/episodes?page[limit]=20&page[offset]=${Anime_api.page}`
             contenido = function showEpisodios(info){
                          info = info.data
                         let html = ""
                         console.log(info)
-                        info.forEach((el) => {
-                            html += episodes(el)
-                         })   
-                        $tabcontentEp.insertAdjacentHTML("beforeend",html) 
+                        if(info.length === 0){
+                            return false;
+                        }else{
+                            info.forEach((el) => {
+                                html += episodes(el)
+                             })   
+                            $tabcontentEp.insertAdjacentHTML("beforeend",html)
+                        }
+                         
         }
 
-        if(scrollTop + clientHeight >= scrollHeight){
+        document.querySelector(".loader").style.display = "block;"
+
             await ajax({
                 url: direccionURL,
                 cbSuccess:(data) => {
@@ -41,6 +48,6 @@ export async function infiniteScroll(){
                 }
             })
         }
-    }
+      }
     })
 }
