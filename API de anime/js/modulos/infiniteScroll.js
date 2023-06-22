5c2e43870cb67e4ce8839c12fce7caf223bcfa51
BrandonLunaCastro <blunacastro2003@gmail.com>
2023-06-22 10:56:06 -0300

    Update infiniteScroll.js

--- API de anime/js/modulos/infiniteScroll.js
+++ API de anime/js/modulos/infiniteScroll.js
@@ -1,28 +1,46 @@
-import ajax from "./ajax";
+import Anime_api from "./Anime_api.js";
+import ajax from "./ajax.js";
+import episodes from "./episodios.js";
 
-export async function infiniteScroll(url){
-    window.addEventListener("scroll",e => {
+export async function infiniteScroll(){
+    console.log("entra a infinit scroll")
+    window.addEventListener("scroll", async e => {
         let {scrollTop,scrollHeight,clientHeight} = document.documentElement,
         direccionURL,
+        contenido,
         id = localStorage.getItem("PostId"),
-        page=0
+        $tabcontentPj = document.getElementById("personajesContent"),
+        $tabcontentEp = document.getElementById("episodiosContent")
+
+        Anime_api.page = 0
 
         if(location.hash === "#/personajes"){
-            page+=20
-            direccionURL = `https://kitsu.io/api/edge/anime/${id}/characters?page[limit]=20&page[offset]=${page}`
+            Anime_api.page+=20
+            direccionURL = `https://kitsu.io/api/edge/anime/${id}/characters?page[limit]=20&page[offset]=${Anime_api.page}`
+            contenido = function showPersonaje(){
+
+            }
         }else if(location.hash === "#/episodios"){
-            page
+            Anime_api.page+=20
+            direccionURL = `https://kitsu.io/api/edge/anime/${id}/episodes?page[limit]=20&page[offset]=${Anime_api.page}`
+            contenido = function showEpisodios(info){
+                         info = info.data
+                        let html = ""
+                        console.log(info)
+                        info.forEach((el) => {
+                            html += episodes(el)
+                         })   
+                        $tabcontentEp.insertAdjacentHTML("beforeend",html) 
         }
 
         if(scrollTop + clientHeight >= scrollHeight){
-            ajax({
+            await ajax({
                 url: direccionURL,
-                cbSuccess:(personajes) =>{
-    
+                cbSuccess:(data) => {
+                    contenido(data)
                 }
             })
-    
         }
-        
+    }
     })
 }
