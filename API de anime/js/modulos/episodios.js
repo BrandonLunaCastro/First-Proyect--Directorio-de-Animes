b86796d6a7860204793420a0542b8fb4620f7dea
BrandonLunaCastro <110911675+BrandonLunaCastro@users.noreply.github.com>
2023-06-23 17:39:12 -0300

    Update episodios.js

--- API de anime/js/modulos/episodios.js
+++ API de anime/js/modulos/episodios.js
@@ -2,8 +2,11 @@ export default function episodes(props){
     //console.log(props.attributes)
     let {canonicalTitle,thumbnail,number} = props.attributes,
     img = thumbnail ? thumbnail.original : "assets/files/no-image-available.jpeg",
-    titulo =  canonicalTitle.length > 20 ? canonicalTitle.slice(0,20)+"..." : canonicalTitle;
-   
+    titulo = canonicalTitle 
+                   ? canonicalTitle.length > 20 
+                   ? canonicalTitle.slice(0,20)+"..." 
+                   : canonicalTitle
+                   : "No hay titulo disponible"
     return `
     <article class="container-episodes">
         <img src="${img}" alt="${titulo}">
