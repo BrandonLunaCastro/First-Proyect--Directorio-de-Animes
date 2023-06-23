7f655f8f03ac2648b956e80c24fb378870041b75
BrandonLunaCastro <blunacastro2003@gmail.com>
2023-06-23 11:16:26 -0300

    Update infiniteScroll.js

--- API de anime/js/modulos/infiniteScroll.js
+++ API de anime/js/modulos/infiniteScroll.js
@@ -5,60 +5,68 @@ import episodes from "./episodios.js";
 
 export async function infiniteScroll(){
     console.log("entra a infinit scroll")
+    
     window.addEventListener("scroll", async e => {
         let {scrollTop,scrollHeight,clientHeight} = document.documentElement,
         direccionURL,
         contenido,
         id = localStorage.getItem("PostId"),
         $tabcontentPj = document.getElementById("personajesContent"),
-        $tabcontentEp = document.getElementById("episodiosContent")
+        $tabcontentEp = document.getElementById("episodiosContent"),
+        $loader = document.querySelector(".loader");
+
        console.log(Anime_api.page)
        
         if(scrollTop + clientHeight >= scrollHeight){
            Anime_api.page+=20
-        
+
+           
          if(location.hash === "#/personajes"){    
-            console.log("entro en personajes")
+          
+            //console.log("entro en personajes")
             direccionURL = `https://kitsu.io/api/edge/anime/${id}/characters?page[limit]=20&page[offset]=${Anime_api.page}`
             contenido = function showPersonaje(info){
                 info = info.data 
                 console.log(info,"aqui se ve que trae")
                 if(info.length === 0){                    
-                    $tabcontentPj.innerHTML = `<p class="vacio">Ups parece que aún no hay nada cargado aqui  ¯\_(ツ)_/¯</p>`
+                    $tabcontentPj.innerHTML = `<p class="vacio">Ups parece que aún no hay nada cargado aqui  ¯\_(ツ)_/¯</p>`                   
                     return false;
                 }else {
+                    $loader.style.display = "block";
                      info.forEach((el)=>{ 
                         character(el)
                      }) 
                 }          
             }
-        } 
-        if(location.hash === "#/episodios"){
-            console.log("entro en episodios")
+        }else if(location.hash === "#/episodios"){           
+            $loader.style.display = "block";
             direccionURL = `https://kitsu.io/api/edge/anime/${id}/episodes?page[limit]=20&page[offset]=${Anime_api.page}`
             contenido = function showEpisodios(info){
                          info = info.data
                         let html = ""
                         console.log(info)
-                        if(info.length === 0){
-                            console.log("vacio")
-                            return false;
+                        if(info.length === 0){                      
+                            $loader.style.display ="none";
                         }else{
+                                                
                             info.forEach((el) => {
                                 html += episodes(el)
                              })   
                             $tabcontentEp.insertAdjacentHTML("beforeend",html)
-                        }
-                         
-        }
+                            
+                        }}
+            }else {
+            $tabcontentEp.innerHTML = null
+            $tabcontentPj.innerHTML = null
+            return false;
         }
 
-        document.querySelector(".loader").style.display = "block"
-            
+        
         await ajax({
             url: direccionURL,
             cbSuccess:(data) => {
                contenido(data)
+            
             }
         })
     
