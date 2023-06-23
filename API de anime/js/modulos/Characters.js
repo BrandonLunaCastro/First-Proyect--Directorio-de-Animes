c338295743bc18f7375a7679ab38c7455385652b
BrandonLunaCastro <110911675+BrandonLunaCastro@users.noreply.github.com>
2023-06-23 19:39:12 -0300

    Update Characters.js

--- API de anime/js/modulos/Characters.js
+++ API de anime/js/modulos/Characters.js
@@ -4,11 +4,7 @@ import { personaje } from "./personaje.js";
 export default   function  character(elements){
         let {id} = elements,
         $tabcontent = document.getElementById("personajesContent")
-        
-        if(id.length === 0){
-            document.querySelector(".loader").style.display = "none"
-        }
-
+    
         ajax({
             url:`https://kitsu.io/api/edge/media-characters/${id}/character`,
             cbSuccess:(persona)=>{
@@ -20,7 +16,6 @@ export default   function  character(elements){
                 let {scrollTop,scrollHeight,clientHeight} = document.documentElement;
                     if(scrollTop+clientHeight >= scrollHeight){
                         const newTargeta = personaje(data.attributes)
-                        console.log(newTargeta)
                         $tabcontent.insertAdjacentHTML("beforeend",newTargeta)
                     }
                 }) 
