114df8fd3cc6b7a781f14b6eeaf34f00764dc1e7
BrandonLunaCastro <110911675+BrandonLunaCastro@users.noreply.github.com>
2023-06-24 18:48:44 -0300

    Update infiniteScroll.js

--- API de anime/js/modulos/infiniteScroll.js
+++ API de anime/js/modulos/infiniteScroll.js
@@ -5,29 +5,27 @@ import episodes from "./episodios.js";
 
 export async function infiniteScroll(url){
     console.log("entra a infinit scroll")
-    
-    window.addEventListener("scroll", async e => {
-        let {scrollTop,scrollHeight,clientHeight} = document.documentElement,
-        direccionURL,
-        contenido,
-        id = localStorage.getItem("PostId"),
-        $tabcontentPj = document.getElementById("personajesContent"),
-        $tabcontentEp = document.getElementById("episodiosContent"),
-        $loader = document.querySelector(".loader"),
-        vacio = false;
+    let   direccionURL,
+    contenido,
+    id = localStorage.getItem("PostId"),
+    $tabcontentPj = document.getElementById("personajesContent"),
+    $tabcontentEp = document.getElementById("episodiosContent"),
+    $loader = document.querySelector(".loader")
 
-        if(scrollTop + clientHeight >= scrollHeight){
-            Anime_api.page+=20  
-           
+    window.addEventListener("scroll", async e => {
+   
+        let {scrollTop,scrollHeight,clientHeight} = document.documentElement
+
+        if(scrollTop + clientHeight >= scrollHeight - 7){
+            Anime_api.page+=20             
          if(location.hash === "#/personajes"){        
-            $loader.style.display = "block" 
           console.log("entro a personajes")
             direccionURL = `https://kitsu.io/api/edge/anime/${id}/characters?page[limit]=20&page[offset]=${Anime_api.page}`
             contenido = function showPersonaje(info){
                 info = info.data 
                 console.log(info)
                      info.forEach((el)=>{ 
-                        character(el);
+                        character(el,true);
                      }); 
             }
         }else if(location.hash === "#/episodios"){       
@@ -39,13 +37,15 @@ export async function infiniteScroll(url){
                             html += episodes(el)
                          })   
                         $tabcontentEp.insertAdjacentHTML("beforeend",html)
-                        }
+            }
             } else {
              $tabcontentEp.innerHTML = null
              $tabcontentPj.innerHTML = null
              return false;
             }   
 
+
+      
                 await ajax({
                     url: direccionURL,
                     cbSuccess:(data) => {
@@ -57,7 +57,7 @@ export async function infiniteScroll(url){
                     }    
                     }
                 });             
-                $loader.style.display="none";
+   
         }
     })
 }
