1715b98bb3d0a75caacd1b44b9e1320b44f87e0a
BrandonLunaCastro <110911675+BrandonLunaCastro@users.noreply.github.com>
2023-06-24 15:59:25 -0300

    Update infiniteScroll.js

--- API de anime/js/modulos/infiniteScroll.js
+++ API de anime/js/modulos/infiniteScroll.js
@@ -3,7 +3,7 @@ import character from "./Characters.js";
 import ajax from "./ajax.js";
 import episodes from "./episodios.js";
 
-export async function infiniteScroll(){
+export async function infiniteScroll(url){
     console.log("entra a infinit scroll")
     
     window.addEventListener("scroll", async e => {
@@ -14,68 +14,50 @@ export async function infiniteScroll(){
         $tabcontentPj = document.getElementById("personajesContent"),
         $tabcontentEp = document.getElementById("episodiosContent"),
         $loader = document.querySelector(".loader"),
-        estado = true
-
-       console.log(scrollHeight)
-       
+        vacio = false;
 
         if(scrollTop + clientHeight >= scrollHeight){
-            Anime_api.page+=20
-            
-         if(location.hash === "#/personajes"){    
+            Anime_api.page+=20  
+           
+         if(location.hash === "#/personajes"){        
+            $loader.style.display = "block" 
+          console.log("entro a personajes")
             direccionURL = `https://kitsu.io/api/edge/anime/${id}/characters?page[limit]=20&page[offset]=${Anime_api.page}`
             contenido = function showPersonaje(info){
                 info = info.data 
                 console.log(info)
-                if(info.length === 0){                    
-                    estado = false;
-                }else {
                      info.forEach((el)=>{ 
-                      character(el);
+                        character(el);
                      }); 
-                }          
             }
-        }
-         
-        if(location.hash === "#/episodios"){                 
+        }else if(location.hash === "#/episodios"){       
             direccionURL = `https://kitsu.io/api/edge/anime/${id}/episodes?page[limit]=20&page[offset]=${Anime_api.page}`
             contenido = function showEpisodios(info){
                          info = info.data
                         let html = ""
-                        console.log(info)
-                        if(info.length === 0){                                         
-                                estado = false;
-                        }else{                 
-                            info.forEach((el) => {
-                                html += episodes(el)
-                             })   
-                            $tabcontentEp.insertAdjacentHTML("beforeend",html)
-                        }}
-            }else {
-            $tabcontentEp.innerHTML = null
-            $tabcontentPj.innerHTML = null
-            estado = false;
-        }
-            console.log(estado)
-            
-            if(estado !==false ){
-                $loader.style.display="block";
+                        info.forEach((el) => {
+                            html += episodes(el)
+                         })   
+                        $tabcontentEp.insertAdjacentHTML("beforeend",html)
+                        }
+            } else {
+             $tabcontentEp.innerHTML = null
+             $tabcontentPj.innerHTML = null
+             return false;
+            }   
+
                 await ajax({
                     url: direccionURL,
                     cbSuccess:(data) => {
-                       contenido(data)    
-                       $loader.style.display="none";
+                    if(data.data.length === 0){
+                        console.log("entro a vacio")
+                        return false;
+                    }else{
+                       return contenido(data);    
+                    }    
                     }
-                })             
-            }else{
-                $loader.style.display="block";   
-            }
-   
-       
-    
+                });             
+                $loader.style.display="none";
         }
-        
-     
-
     })
 }
